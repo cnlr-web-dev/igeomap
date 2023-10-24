0e324c9a4db22398b1eaf2bfb9fb870da6a8abb2
Moldytzu <m.alex123@yahoo.com>
2023-10-24 18:13:50 +0300

    stlExporter: prototip exporter suprafata

--- dataset/stlExporter.c
+++ dataset/stlExporter.c
@@ -33,51 +33,57 @@ hgt_file_t *hgt;
 int16_t max = HGT_DATA_VOID, min = INT16_MAX;
 uint64_t avg = 0;
 
+// suprafata terenului
+int16_t suprafata[4000][4000];
+size_t arie;
+size_t factor = 3;
+
 void genereazaSTL()
 {
-    const int TRIUNGHIURI = 1000;                                              // un triunghi (de moment)
-    stl_triangle_t *triunghiuri = calloc(TRIUNGHIURI, sizeof(stl_triangle_t)); // alocam dinamic triunghiurile
-    stl_header_t *antet = calloc(1, sizeof(stl_header_t));                     // alocam dinamic antetul
+    int latura = hgt->sideLength / factor;                                       // latura impartita cu factor
+    int nrTriunghiuri = latura * latura;                                         // aria noului patrat
+    stl_triangle_t *triunghiuri = calloc(nrTriunghiuri, sizeof(stl_triangle_t)); // alocam dinamic triunghiurile
+    stl_header_t *antet = calloc(1, sizeof(stl_header_t));                       // alocam dinamic antetul
 
     // setam numarul de triunghiuri in antet
-    antet->triunghiuri = TRIUNGHIURI;
+    antet->triunghiuri = nrTriunghiuri;
 
-    const float32_t apropiere = 0.001;
-    for (float32_t i = 0; i < TRIUNGHIURI; i++)
+    // generam suprafata
+    for (int x = 1; x < latura; x++)
     {
-        stl_triangle_t *triunghi = &triunghiuri[(int)i];
-        triunghi->normal.x = triunghi->normal.y = triunghi->normal.z = -1;
+        for (int y = 1; y < latura; y++)
+        {
+            stl_triangle_t *triunghi = &triunghiuri[y * latura + x];
+            triunghi->normal.x = triunghi->normal.y = triunghi->normal.z = 0;
 
-        float32_t punctX = i * apropiere, punctY = i * apropiere;
+            triunghi->varf[0].x = x + 1;
+            triunghi->varf[0].y = y + 1;
+            triunghi->varf[0].z = suprafata[x + 1][y + 1];
 
-        triunghi->varf[0].x = 0;
-        triunghi->varf[0].y = 0;
+            triunghi->varf[1].x = x;
+            triunghi->varf[1].y = y;
+            triunghi->varf[1].z = suprafata[x][y];
 
-        triunghi->varf[1].x = punctX;
-        triunghi->varf[1].y = punctY;
-
-        triunghi->varf[2].x = 1;
-        triunghi->varf[2].y = 1;
-
-        triunghi->varf[0].z = 0;
-        triunghi->varf[1].z = i;
-        triunghi->varf[2].z = 0;
+            triunghi->varf[2].x = x - 1;
+            triunghi->varf[2].y = y - 1;
+            triunghi->varf[2].z = suprafata[x - 1][y - 1];
+        }
     }
 
     // scrie in fisier
     FILE *fd = fopen("test.stl", "wb");
-    fwrite(antet, sizeof(stl_header_t), 1, fd);                   // scrie antetul
-    fwrite(triunghiuri, sizeof(stl_triangle_t), TRIUNGHIURI, fd); // scrie triunghiurile
+    fwrite(antet, sizeof(stl_header_t), 1, fd);                     // scrie antetul
+    fwrite(triunghiuri, sizeof(stl_triangle_t), nrTriunghiuri, fd); // scrie triunghiurile
     fclose(fd);
 }
 
 int main()
 {
-    hgt = hgtCreateContext("raw/N48E029.hgt");       // creeaza context pentru fisierul hgt
-    size_t arie = hgt->sideLength * hgt->sideLength; // aria perimetrului descris
+    hgt = hgtCreateContext("raw/N48E029.hgt"); // creeaza context pentru fisierul hgt
+    arie = hgt->sideLength * hgt->sideLength;  // aria perimetrului descris
 
-    // cautam extremele elevatiei in tot fisierul
-    for (size_t i = 0; i < arie; i++)
+    // cautam extremele elevatiei in tot fisierul si integram in matrice
+    for (size_t i = 0; i < arie; i += factor)
     {
         int16_t elevatie = hgtReadElevationRaw(hgt, i);
         if (elevatie > max)
@@ -86,9 +92,11 @@ int main()
             min = elevatie;
 
         avg += abs(elevatie);
+
+        suprafata[(i / factor) / arie][(i / factor) % arie] = elevatie;
     }
     avg /= arie; // facem media tuturor punctelor
-    printf("%s: min: %dm max: %dm avg: %ldm\n", hgt->filename, min, max, avg);
+    printf("%s: %dx%d min: %dm max: %dm avg: %ldm\n", hgt->filename, hgt->sideLength, hgt->sideLength, min, max, avg);
 
     genereazaSTL(); // fa generarea
 
